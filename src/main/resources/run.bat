ac850a9f86645e959374ca5f6eb7948ce40bd574
zhaosg <>
2015-09-16 13:53:51 +0800

    出事提交

--- src/main/resources/run.bat
+++ src/main/resources/run.bat
@@ -1,2 +1,10 @@
+java -jar mybatis-util-0.0.1-SNAPSHOT.jar  -clear
 java -jar mybatis-util-0.0.1-SNAPSHOT.jar  -tableName t_goods -tablePrefix t
 java -jar mybatis-util-0.0.1-SNAPSHOT.jar  -tableName t_goods_common -tablePrefix t
+java -jar mybatis-util-0.0.1-SNAPSHOT.jar  -tableName t_goods_attribute -tablePrefix t
+java -jar mybatis-util-0.0.1-SNAPSHOT.jar  -tableName t_goods_browse -tablePrefix t
+java -jar mybatis-util-0.0.1-SNAPSHOT.jar  -tableName t_goods_consult -tablePrefix t
+java -jar mybatis-util-0.0.1-SNAPSHOT.jar  -tableName t_goods_evaluate -tablePrefix t
+java -jar mybatis-util-0.0.1-SNAPSHOT.jar  -tableName t_goods_images -tablePrefix t
+java -jar mybatis-util-0.0.1-SNAPSHOT.jar  -tableName t_goods_tag -tablePrefix t
+java -jar mybatis-util-0.0.1-SNAPSHOT.jar  -tableName t_goods_type -tablePrefix t
